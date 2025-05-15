d6fa0d87a6a0f723bfeb4505b207fc9ff5a60279
zoehsu1203 <zoehsu1203@gmail.com>
2025-05-15 18:56:36 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CostCalculator from './components/CostCalculator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <CostCalculator />
+    </div>
+  );
+}
+
+export default App;
